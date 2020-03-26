511940b5fca66bc82aec90bc4163a251c2be511b
strengthofthemountain <strengthofthemountain@gmail.com>
2020-03-26 11:21:04 -0400

    mvp

--- useronboarding-app/src/App.js
+++ useronboarding-app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Form from './Components/Form.js';
+import Form from './components/Form';
 
 function App() {
   return (
